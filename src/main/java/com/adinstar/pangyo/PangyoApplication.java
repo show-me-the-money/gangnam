4bef562411c0c5021f4afce32abffc3c89f8d052
betty.yun <betty.yun@daumkakao.com>
2018-01-08 15:30:37 +0900

    spring boot start!!!

---
+++ src/main/java/com/adinstar/pangyo/PangyoApplication.java
@@ -0,0 +1,12 @@
+package com.adinstar.pangyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PangyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PangyoApplication.class, args);
+	}
+}
